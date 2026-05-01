8089715a8dea6b642743385ad79b9b5604a131ff
JI-Siam <jafirislam10@gmail.com>
2026-05-01 23:53:34 +0600

    Auth setup and React_Hook

--- neuro-learn/src/components/shared/auth/LoginForm.jsx
+++ neuro-learn/src/components/shared/auth/LoginForm.jsx
@@ -1,20 +1,36 @@
+'use client'
 import React from 'react';
 import Link from 'next/link';
+import { useForm, SubmitHandler } from "react-hook-form"
+
 const LoginForm = () => {
+     const { register, handleSubmit , formState: { errors } } = useForm()
+
+    const handleLogin = (data)=> {
+        console.log(data , "data") ; 
+        console.log(errors , "errors") ;
+    }
     return (
         <div className='flex justify-center items-center my-20'>
+            <form action="" onSubmit={handleSubmit(handleLogin)}>
+
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                     <legend className="fieldset-legend">Login</legend>
 
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                    <input type="email" {...register("email" , { required: "*Email Required" })} className="input" placeholder="Email" />
+                    {errors.email && <p className='text-red-700'>{errors.email.message}</p>}
 
                     <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="password"  {...register("password" ,  { required: "*Password Required" })} className="input" placeholder="Password" />
+                    {errors.password && <p className='text-red-700'>{errors.password.message}</p>}
 
-                    <button className="btn btn-neutral mt-4">Login</button>
+                    <input type="submit" className="btn btn-neutral mt-4" value="Login"/>
                     <p> Don't Have an Accout?  <Link href="\signup" className='text-primary font-bold'>Register Here</Link></p> 
                 </fieldset>
+
+            </form>
+               
         </div>
     );
 };
--- neuro-learn/src/components/shared/auth/RegistrationForm.jsx
+++ neuro-learn/src/components/shared/auth/RegistrationForm.jsx
@@ -1,27 +1,45 @@
+'use client'
 import React from 'react';
 import Link from 'next/link';
+import { useForm, SubmitHandler } from "react-hook-form"
 
 const RegistrationForm = () => {
+     const { register, handleSubmit , formState: { errors } } = useForm()
+    
+     const handleRegistration = (data)=> {
+        console.log(data , "data") ; 
+        console.log(errors , "errors") ;
+    }
     return (
           <div className='flex justify-center items-center my-20'>
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+            <form action="" onSubmit={handleSubmit(handleRegistration)}>
+                   <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                     <legend className="fieldset-legend">Register</legend>
 
-                     <label className="label">Name</label>
-                    <input type="text" className="input" placeholder="Name" />
+                    <label className="label">Name</label>
+                    <input type="text" {...register("name" , { required: "*Name Required" })} className="input" placeholder="Name" />
+                      {errors.name && <p className='text-red-700'>{errors.name.message}</p>}
 
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                    <input type="email" {...register("email" , { required: "*Email Required" })}  className="input" placeholder="Email" />
+                          {errors.email && <p className='text-red-700'>{errors.email.message}</p>}
 
                      <label className="label">Photo URL</label>
-                    <input type="text" className="input" placeholder="Photo URL" />
+                    <input type="text"  {...register("url" , { required: "*URL Required" })} className="input" placeholder="Photo URL" />
+                          {errors.url && <p className='text-red-700'>{errors.url.message}</p>}
 
                     <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="password"  {...register("password" , { required: "*Password Required" })} className="input" placeholder="Password" />
+                          {errors.password && <p className='text-red-700'>{errors.password.message}</p>}
 
-                    <button className="btn btn-neutral mt-4">Register</button>
+
+                    <input type="submit" className="btn btn-neutral mt-4" value="Register"/>
+                
                      <Link href="\login" className='text-primary text-center font-bold'>Login</Link>
                 </fieldset>
+            </form>
+             
         </div>
     );
 };
